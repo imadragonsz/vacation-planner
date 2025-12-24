4c543bff36a4084ca50f2f76aa719332688b587f
unknown <mittchelk@gmail.com>
2025-12-24 21:49:11 +0100

    hi

--- src/assets/markerIcons.ts
+++
@@ -1,9 +0,0 @@
-// Moved from src\markerIcons.ts
-
-// Define your marker icons here
-const markerIcons = {
-  default: "path/to/default/icon.png",
-  // add other marker types here
-};
-
-export default markerIcons;
